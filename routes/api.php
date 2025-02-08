5b1f3d2c0bb752e1f2e1ba05d149c6fa4ba6c34d
haristanone <Tanoneharis@gmail.com>
2025-02-08 21:40:30 +0700

    update privilage

--- routes/api.php
+++ routes/api.php
@@ -17,14 +17,31 @@ Route::post('login', [AuthController::class, 'login'])->name('login');
 
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('categories', CategoryController::class);
-    Route::post('categories/{id}/restore', [CategoryController::class, 'restore']);
 
-    Route::apiResource('books', BookController::class);
-    Route::post('books/{id}/restore', [BookController::class, 'restore']);
+    Route::get('categories', [CategoryController::class, 'index']);
+    Route::get('categories/{id}', [CategoryController::class, 'show']);
+
+    Route::middleware('role:admin')->group(function () {
+        Route::post('categories', [CategoryController::class, 'store']);
+        Route::put('categories/{id}', [CategoryController::class, 'update']);
+        Route::delete('categories/{id}', [CategoryController::class, 'destroy']);
+        Route::post('categories/{id}/restore', [CategoryController::class, 'restore']);
+    });
+
+    Route::get('books', [BookController::class, 'index']);
+    Route::get('books/{id}', [BookController::class, 'show']);
+
+    Route::middleware('role:admin')->group(function () {
+        Route::post('books', [BookController::class, 'store']);
+        Route::put('books/{id}', [BookController::class, 'update']);
+        Route::delete('books/{id}', [BookController::class, 'destroy']);
+        Route::post('books/{id}/restore', [BookController::class, 'restore']);
+    });
 
     Route::get('/loans', [LoanController::class, 'index']);
-    Route::post('/loans', [LoanController::class, 'store']);
-    Route::post('/loans/{id}/returnbook', [LoanController::class, 'rBook']);
 
+    Route::middleware('role:admin')->group(function () {
+        Route::post('/loans', [LoanController::class, 'store']);
+        Route::post('/loans/{id}/returnbook', [LoanController::class, 'rBook']);
+    });
 });
