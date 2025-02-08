5d5fc5eb38fc855a1a3fde61d44ea73203c20972
haristanone <Tanoneharis@gmail.com>
2025-02-08 14:38:38 +0700

    add model and migration

---
+++ app/Models/Loan.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Loan extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'book_id',
+        'loan_date',
+        'return_date',
+        'status'
+    ];
+
+    protected $casts = [
+        'loan_date' => 'date',
+        'return_date' => 'date',
+    ];
+
+    protected $dates = ['deleted_at'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function book()
+    {
+        return $this->belongsTo(Book::class);
+    }
+
+    public function bookReturn()
+    {
+        return $this->hasOne(BookReturn::class);
+    }
+}
