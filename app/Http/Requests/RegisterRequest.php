a091280ee1ced503e3ff4535e66224a1c7b78c35
haristanone <Tanoneharis@gmail.com>
2025-02-08 13:55:34 +0700

    update register validation

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -24,7 +24,7 @@ class RegisterRequest extends FormRequest
     {
         return [
             'name' => 'required|string|max:100',
-            'email' => 'required|email',
+            'email' => 'required|email|unique:users,email',
             'password' => 'required|min:5'
         ];
     }
